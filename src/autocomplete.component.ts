7bb32319af2e07613bd705e6245d11ccc3545728
Trung Nguyen <trungnv184@gmail.com>
2018-07-30 15:58:19 +0700

    change click event to tap event due to resolve propagation event issue.

--- src/autocomplete.component.ts
+++ src/autocomplete.component.ts
@@ -65,7 +65,7 @@ const defaultOpts = {
             <span [innerHTML]='attrs.label | boldprefix:attrs.keyword'></span>
         </ng-template>
         <ul *ngIf="!disabled && suggestions.length > 0 && showList">
-            <li *ngFor="let suggestion of suggestions" (tap)="handleSelectTap($event, suggestion)">
+            <li *ngFor="let suggestion of suggestions" (click)="select(suggestion);$event.stopPropagation()">
                 <ng-template
                         [ngTemplateOutlet]="template || defaultTemplate"
                         [ngTemplateOutletContext]="
